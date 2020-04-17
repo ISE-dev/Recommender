dfebf09dd9117d205345d469daf7ad4d8bbc7d79
nkue-yst <yoshi_frederic@yahoo.co.jp>
2020-04-17 22:52:05 +0900

    Add users and post table. #3

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ composer update
 cp .env.example .env
 php artisan key:generate
 npm install
+touch ./database/database.sqlite
+php artisan migrate
 echo "The setup is complete."
 echo "Please type 'php artisan serve' to start server."
