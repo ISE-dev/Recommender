cdcb12ce8edf86fe1db55853d03747918d5abafe
nkue-yst <g1853581@cc.kyoto-su.ac.jp>
2020-04-22 23:25:14 +0900

    Add user samples.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+echo "[System]: Update composer."
 composer update
+
+echo "[Sytsem]: Setup server."
 cp .env.example .env
 php artisan key:generate
-npm install
 touch ./database/database.sqlite
 php artisan migrate
+php artisan db:seed
+
+echo "[System]: Setup npm."
+npm install
+
 echo "The setup is complete."
-echo "Please type 'php artisan serve' to start server."
+echo "Please type 'npm run watch' & 'php artisan serve' to start server."
