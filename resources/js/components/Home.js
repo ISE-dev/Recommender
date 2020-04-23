be63abfe8649cd88fc3170bd553017548a6f6df4
eiki <53928021+N-Eiki@users.noreply.github.com>
2020-04-23 14:53:31 +0900

    rewite_by_react (#13)
    * rewite_to_react
    
    * class => className

---
+++ resources/js/components/Home.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+ export class Header extends React.Component {
+    render() {
+      return (<header id="top">
+       <div className="header">
+          <div className="header-left">
+           <h1 className="title">App title</h1>
+          </div>
+  
+         <div className="header-right">
+           <a href="#" className="signup link">新規登録</a><a href="#" className="login link">ログイン</a>
+         </div>
+  
+          <div className="header-form">
+           <form>
+             <button> 🔍</button>
+             <input type="search" placeholder="キーワードを入力"></input>        
+           </form>
+         </div>      
+       </div>
+     </header>);
+    }
+  }
+
+  export class Message extends React.Component{
+    render(){
+      return (
+        <div>
+          <h1 className="announce">recent post</h1>
+        </div>
+      );
+    }
+  }
+  
+
+  export class Content extends React.Component {
+    constructor(props){
+      super(props);
+      this.state=["a","b","c","d","e","f","g","h","i","j","k","l","m","n"];
+    }
+    render() {
+      return (
+        <div className="content">
+                {Object.keys(this.state).map(key => (
+            <div className="RecommendedItem">
+              <h1>
+                記事　{this.state[key]}
+              </h1>
+              <h5>・おすすめ</h5>
+              <p>おすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめ
+                おすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめおすすめ</p>
+            </div>          
+          ))}
+        </div>
+      );  
+    }
+  }
+  
+
+ export class GoTop extends React.Component {
+    render() {
+      return (
+        <div>
+          <nav>
+            <a href="#top" className="go-top" go top>&and;</a>  
+          </nav>
+        </div>
+      );
+    }
+  }
+  
