fd5cac69af8ef837d78200829cca42f2980ea39c
yossyi <g1853581@cc.kyoto-su.ac.jp>
2020-04-23 15:08:24 +0900

    Add data samples. (#12)
    * Add user samples.
    
    * Add post samples.
    
    * Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo "[System]: Update composer."
 composer update
+
+echo "[Sytsem]: Setup server."
 cp .env.example .env
 php artisan key:generate
-npm install
+FILE="./database/database.sqlite"
+if [ -e $FILE ]; then
+  rm ./database/database.sqlite
+fi
 touch ./database/database.sqlite
 php artisan migrate
+php artisan db:seed
+
+echo "[System]: Setup npm."
+npm install
+
 echo "The setup is complete."
-echo "Please type 'php artisan serve' to start server."
+echo "Please type 'npm run watch' & 'php artisan serve' to start server."
